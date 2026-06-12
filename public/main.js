06c8cacaddff33118f01cd61fdf6119c113be56e
erinc basar <takmasakal@gmail.com>
2026-06-12 11:57:53 +0300

    Update TRT Belgelik login branding

--- public/main.js
+++ public/main.js
@@ -245,7 +245,7 @@ function showShortcutToast(message) {
 
 let i18n = {
   en: {
-    app_title: 'MAM Console',
+    app_title: 'TRT Belgelik',
     app_subtitle: 'Dalet-style MVP: ingest, metadata, versions',
     current_user: 'Current User',
     user_loading: 'Loading...',
@@ -580,7 +580,7 @@ let i18n = {
     subtitle_overlay_enabled: 'Show subtitles'
   },
   tr: {
-    app_title: 'MAM Konsolu',
+    app_title: 'TRT Belgelik',
     app_subtitle: 'Dalet benzeri MVP: ingest, metadata, iş akışı, versiyonlar',
     current_user: 'Giriş yapan',
     user_loading: 'Yükleniyor...',
